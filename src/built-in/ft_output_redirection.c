38114290e7109ef199ba376dca4e8f0483787c60
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-03-25 15:00:46 +0100

    Create two redirection functions

---
+++ src/built-in/ft_output_redirection.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_output_redirection.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 00:08:11 by vcordeir          #+#    #+#             */
+/*   Updated: 2022/03/25 03:12:30 by vcordeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/shell.h"
+
+void	ft_output_redirection(char *file_path, int mode, char *txt)
+{
+	int	fd;
+	int	flag;
+	
+	if (mode == ONE_CHAR)
+		flag = O_WRONLY;
+	else if (mode == TWO_CHARS)
+		flag = O_APPEND;
+
+	fd = open(file_path, flag | O_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	write(fd, txt, ft_strlen(txt));
+	close(fd);
+}
