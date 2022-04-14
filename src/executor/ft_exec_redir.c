e23edc335a3e97a5a4405cdb39f09c718aaede00
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-04-14 14:05:15 +0200

    Redirect output almost working

--- src/executor/ft_exec_redir.c
+++ src/executor/ft_exec_redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exec_redir.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chideyuk <chideyuk@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 19:32:56 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/04/13 20:02:37 by chideyuk         ###   ########.fr       */
+/*   Updated: 2022/04/14 14:03:51 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,27 @@
 
 void	ft_exec_redir(t_shell *mshell, char **env, t_cmd_table *cmdtable)
 {
-	int	pid;
-	int	fd[2];
-	int	out;
-	char	*line;
+	int	fdout;
+	int flag_permission;
+	int fdpipe[2];
 
-	pipe(fd);
-	pid = fork();
-	if (pid != 0)
-	{
-		close(fd[0]);
-		dup2(fd[1], 1);
-		ft_exec_cmd(mshell, env, 1, cmdtable);
-		close(fd[1]);
-		waitpid(pid, NULL, WUNTRACED);
-	}
-	else if (pid == 0)
-	{
-		close(fd[1]);
-		out = get_next_line(fd[0], &line);
-		ft_output_redirection(cmdtable->output_arg, cmdtable->output_type, line);
-		while (out == 1)
-		{
-			out = get_next_line(fd[1], &line);
-			ft_output_redirection(cmdtable->output_arg, TWO_CHARS, line);
-		}
-		close(fd[0]);
-	}
+	pipe(fdpipe);
+	flag_permission = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if (access(cmdtable->output_arg, F_OK) == 0)
+		fdout = open(cmdtable->output_arg, cmdtable->output_type | O_WRONLY | flag_permission);
+	else
+		fdout = open(cmdtable->output_arg, cmdtable->output_type | O_CREAT | O_WRONLY | flag_permission);
+
+	dup2(fdpipe[1], fdout);
+	ft_exec_cmd(mshell, env, 1, cmdtable);
+	close(fdpipe[0]);
+	close(fdpipe[1]);
 }
 
-void	ft_redirstart(t_shell *mshell, char **env, t_cmd_table *cmdtable)
-{
-	t_cmd_table	*temp;
+// void	ft_redirstart(t_shell *mshell, char **env, t_cmd_table *cmdtable)
+// {
+// 	t_cmd_table	*temp;
 	
-	temp = cmdtable;
+// 	temp = cmdtable;
 	
-}
+// }
