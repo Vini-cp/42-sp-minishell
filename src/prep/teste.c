8f1cccc60a7a93f1b311a3c6d970ca7b4852234f
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-04-17 03:30:03 +0200

    Norminette prep files

--- src/prep/teste.c
+++ src/prep/teste.c
@@ -6,18 +6,17 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:43:56 by chideyuk          #+#    #+#             */
-/*   Updated: 2022/03/22 23:12:58 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/04/17 03:29:31 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/shell.h"
 
-void ft_printtokens(t_shell *mshell)
+void	ft_printtokens(t_shell *mshell)
 {
 	t_token	*temp;
 
 	temp = mshell->firsttoken;
-
 	while (temp)
 	{
 		printf("TOKEN:%s\n", temp->token);
@@ -27,10 +26,10 @@ void ft_printtokens(t_shell *mshell)
 	return ;
 }
 
-void ft_printtables(t_shell *mshell)
+void	ft_printtables(t_shell *mshell)
 {
 	t_cmd_table	*temp;
-	int i;
+	int			i;
 
 	temp = mshell->cmdtable;
 	while (temp)
@@ -43,7 +42,7 @@ void ft_printtables(t_shell *mshell)
 		if (temp->args != NULL)
 		{
 			i = 0;
-			while(temp->args[i] != NULL)
+			while (temp->args[i] != NULL)
 			{
 				printf("ARGS[%d]: %s\n", i, temp->args[i]);
 				i++;
@@ -60,8 +59,8 @@ void ft_printtables(t_shell *mshell)
 
 void	ft_freeptr(char	**ptr)
 {
-	int 	counter;
-	
+	int	counter;
+
 	counter = 0;
 	while (ptr[counter])
 	{
